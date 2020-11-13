105df9a4414546920cf33792278082f17819217c
lelouchB <ashutoshs.ic18@nsut.ac.in>
2020-11-13 11:49:19 +0530

    Adding Heading to the Gallery

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-// pages/index.js
+//pages/index.js
 import Head from "next/head";
+import { Box, Container, Text } from "@chakra-ui/core";
 
 export default function Home() {
   return (
@@ -8,6 +9,20 @@ export default function Home() {
         <title> NextJS Image Gallery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Box overflow="hidden" bg="purple.100" minH="100vh">
+        <Container>
+          <Text
+            color="pink.800"
+            fontWeight="semibold"
+            mb="1rem"
+            textAlign="center"
+            textDecoration="underline"
+            fontSize={["4xl", "4xl", "5xl", "5xl"]}
+          >
+            NextJS Image Gallery
+          </Text>
+        </Container>
+      </Box>
     </div>
   );
 }
