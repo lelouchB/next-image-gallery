4d4bb45ce4e6caf778404bfe89f264a06e3ff0c1
lelouchB <ashutoshs.ic18@nsut.ac.in>
2020-11-13 12:03:17 +0530

    Styling Images with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import { Box, Container, Text } from "@chakra-ui/core";
+import { Box, Container, Text, Wrap, WrapItem } from "@chakra-ui/core";
 import { getCuratedPhotos } from "../lib/api";
 
 export default function Home({ data }) {
@@ -26,17 +26,20 @@ export default function Home({ data }) {
             NextJS Image Gallery
           </Text>
         </Container>
-        {
-  photos.map((pic) => (
-    <Image
-      src={pic.src.portrait}
-      height={600}
-      width={400}
-      alt={pic.url}
-    />
-  ))
-}
-
+        <Wrap px="1rem" spacing={4} justify="center">
+          {photos.map((pic) => (
+          <WrapItem
+          key={pic.id}
+          boxShadow="base"
+          rounded="20px"
+          overflow="hidden"
+          bg="white"
+          _hover={{ boxShadow: "dark-lg" }}
+        >
+          <Image src={pic.src.portrait} height={600} width={400} alt={pic.url} />
+        </WrapItem>
+          ))}
+        </Wrap>
       </Box>
     </div>
   );
